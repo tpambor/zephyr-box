916a882eab453c8aca5ebe1ba889f9a94fbc8d12
Andrey Dodonov <Andrey.Dodonov@endress.com>
2023-03-20 16:09:44 +0100

    Move Zephyr-box ASCII art to generic entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo `basename "$0"`
 
+printf "\u1b[32m
+███████╗███████╗██████╗ ██╗  ██╗██╗   ██╗██████╗       ██████╗  ██████╗ ██╗  ██╗
+╚══███╔╝██╔════╝██╔══██╗██║  ██║╚██╗ ██╔╝██╔══██╗      ██╔══██╗██╔═══██╗╚██╗██╔╝
+  ███╔╝ █████╗  ██████╔╝███████║ ╚████╔╝ ██████╔╝█████╗██████╔╝██║   ██║ ╚███╔╝ 
+ ███╔╝  ██╔══╝  ██╔═══╝ ██╔══██║  ╚██╔╝  ██╔══██╗╚════╝██╔══██╗██║   ██║ ██╔██╗ 
+███████╗███████╗██║     ██║  ██║   ██║   ██║  ██║      ██████╔╝╚██████╔╝██╔╝ ██╗
+╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝      ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+\033[m\n"
+
 # Move actual logic to another script so that we do not have to rebuild container each time entrypoint.sh is changed
 dos2unix $WORKDIR_CONTAINER/on_docker_startup.sh
 /bin/bash $WORKDIR_CONTAINER/on_docker_startup.sh
