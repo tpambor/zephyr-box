85a8c3f0d722c5c35758d026f3c277e39781b05d
Cla Mattia Galliard <cla-mattia.galliard@zuehlke.com>
2024-10-15 13:32:35 +0200

    run_in_docker: position independent
    Make run_in_docker.sh script independent of the directory it is being
    called from for convenience. Furthermore rename some variables in it for
    clearity.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,46 +13,46 @@ printf "\u1b[32m
 printenv
 
 # Check that the required env variables are set, the others are optional.
-if [[ -z "$PYTHON_VENV_CONTAINER" ]];
+if [[ -z "$PYTHON_VENV" ]];
 then
-    echo "PYTHON_VENV_CONTAINER not defined"
+    echo "PYTHON_VENV not defined"
     exit 1
 fi
-if [[ -z "$WEST_WORKSPACE_CONTAINER" ]];
+if [[ -z "$WEST_WORKSPACE" ]];
 then
-    echo "WEST_WORKSPACE_CONTAINER not defined"
+    echo "WEST_WORKSPACE not defined"
     exit 1
 fi
-if [[ -z "$WORKDIR_CONTAINER" ]];
+if [[ -z "$PROJECT_ROOT" ]];
 then
-    echo "WORKDIR_CONTAINER not defined"
+    echo "PROJECT_ROOT not defined"
     exit 1
 fi
 
 # Create and/or activate python virtual environment
-if [ -d "$PYTHON_VENV_CONTAINER" ];
+if [ -d "$PYTHON_VENV" ];
 then
     echo "Python already initialized"
 else
-    python3 -m venv "$PYTHON_VENV_CONTAINER"
+    python3 -m venv "$PYTHON_VENV"
 fi
-. "$PYTHON_VENV_CONTAINER/bin/activate"
+. "$PYTHON_VENV/bin/activate"
 
 # Initialize and/or update west work space
-cd "$WEST_WORKSPACE_CONTAINER"
+cd "$WEST_WORKSPACE"
 pip3 install west
-if [ -d "$WEST_WORKSPACE_CONTAINER/.west" ];
+if [ -d "$WEST_WORKSPACE/.west" ];
 then
     echo "West already initialized"
 else
-    west init --local "$WORKDIR_CONTAINER"
+    west init --local "$PROJECT_ROOT"
 fi
 west update
 west zephyr-export
 
 # Install pip requirements for zephyr
 pip3 install --verbose --upgrade --no-cache-dir \
-    --requirement "$WEST_WORKSPACE_CONTAINER/zephyr/scripts/requirements.txt"
+    --requirement "$WEST_WORKSPACE/zephyr/scripts/requirements.txt"
 
 # Install pip requirements for project, if set as REQUIREMENTS_TXT
 if [ -f "$REQUIREMENTS_TXT" ];
@@ -77,8 +77,8 @@ fi
 # Execute command passed as arguments in specified directory
 if [[ -z "$CMD_WORK_DIR" ]];
 then
-    CMD_WORK_DIR="$WORKDIR_CONTAINER"
-    echo "No custom working dir specified. defaulting to '$WORKDIR_CONTAINER'"
+    CMD_WORK_DIR="$PROJECT_ROOT"
+    echo "No custom working dir specified. defaulting to '$PROJECT_ROOT'"
 fi
 cd "$CMD_WORK_DIR"
 "$@"
--- run_in_docker.sh
+++ run_in_docker.sh
@@ -4,18 +4,19 @@ set -e
 USER_UID=$(id -u "$(whoami)")
 USER_GID=$(id -g "$(whoami)")
 
-DOCKER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PRJ_ROOT="$(dirname "$DOCKER_DIR")"
-WEST_WORKSPACE_HOST=$PRJ_ROOT/.west_workspace
-USER_HOME_CONTAINER=/home/user
-WEST_WORKSPACE_CONTAINER=$USER_HOME_CONTAINER/west_workspace
+# DOCKER_DIR needs to be the directory containing Dockerfile and a subdirectory of
+# the PROJECT_ROOT_HOST directory
+DOCKER_DIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+PROJECT_ROOT_HOST=$(realpath "$(dirname "$DOCKER_DIR")")
+WEST_WORKSPACE_HOST=$PROJECT_ROOT_HOST/.west_workspace
+HOME_CONTAINTER=/home/user
+WEST_WORKSPACE_CONTAINER=$HOME_CONTAINTER/west_workspace
 PYTHON_VENV_CONTAINER=$WEST_WORKSPACE_CONTAINER/.pyEnv
-WORKDIR_HOST=$PRJ_ROOT
-WORKDIR_CONTAINER=$WEST_WORKSPACE_CONTAINER/$(basename "$PRJ_ROOT")
-REQUIREMENTS_TXT="$WORKDIR_CONTAINER/requirements.txt"
-ON_DOCKER_STARTUP="$WORKDIR_CONTAINER/on_docker_startup.sh"
+PROJECT_ROOT_CONTAINER=$WEST_WORKSPACE_CONTAINER/$(basename "$PROJECT_ROOT_HOST")
+REQUIREMENTS_TXT="$PROJECT_ROOT_CONTAINER/requirements.txt"
+ON_DOCKER_STARTUP="$PROJECT_ROOT_CONTAINER/on_docker_startup.sh"
 
-touch ~/.bash_aliases
+
 mkdir --parents "$WEST_WORKSPACE_HOST"
 
 docker build \
@@ -23,19 +24,17 @@ docker build \
     --build-arg="UID=$USER_UID" \
     --build-arg="GID=$USER_GID" \
     --tag zephyr-box \
-    .
+    "$DOCKER_DIR"
 
 docker run \
     --network host --tty --interactive --rm --privileged \
     --volume "$WEST_WORKSPACE_HOST:$WEST_WORKSPACE_CONTAINER" \
-    --volume "$WORKDIR_HOST:$WORKDIR_CONTAINER" \
-    --volume ~/.ssh:/$USER_HOME_CONTAINER/.ssh \
-    --volume ~/.bash_aliases:$USER_HOME_CONTAINER/.bash_aliases \
+    --volume "$PROJECT_ROOT_HOST:$PROJECT_ROOT_CONTAINER" \
+    --volume ~/.ssh:$HOME_CONTAINTER/.ssh \
     --volume /dev:/dev \
-    --env "WEST_WORKSPACE_CONTAINER=$WEST_WORKSPACE_CONTAINER" \
-    --env "WORKDIR_CONTAINER=$WORKDIR_CONTAINER" \
-    --env "PYTHON_VENV_CONTAINER=$PYTHON_VENV_CONTAINER" \
-    --env "REQUIREMENTS_TXT=$REQUIREMENTS_TXT" \
-    --env "ON_DOCKER_STARTUP=$ON_DOCKER_STARTUP" \
-    zephyr-box \
-    "$@"
+    --env WEST_WORKSPACE="$WEST_WORKSPACE_CONTAINER" \
+    --env PROJECT_ROOT="$PROJECT_ROOT_CONTAINER" \
+    --env PYTHON_VENV="$PYTHON_VENV_CONTAINER" \
+    --env REQUIREMENTS_TXT="$REQUIREMENTS_TXT" \
+    --env ON_DOCKER_STARTUP="$ON_DOCKER_STARTUP" \
+    zephyr-box "$@"
