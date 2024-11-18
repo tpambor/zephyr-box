cc14a9b2d95b5902654b3d113f5c035656cd7b50
Matthias Stutz <matthias.stutz@zuehlke.com>
2024-11-18 12:54:26 +0100

    Customize so that I can be used by CI and local developers

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Parameters to configure script
+SSH_DIR=${SSH_DIR:-"${HOME}/.ssh"}
+RUN_LOCALLY=${RUN_LOCALLY:-"true"}
+RUN_WITH_TTY=${RUN_WITH_TTY:-"true"}
+
+DOCKER_REGISTRY="docker.io/endresshauser"
+IMAGE_NAME="zephyr-box"
+
 USER_UID=$(id -u "$(whoami)")
 USER_GID=$(id -g "$(whoami)")
 
@@ -16,22 +24,44 @@ PROJECT_ROOT_CONTAINER=$WEST_WORKSPACE_CONTAINER/$(basename "$PROJECT_ROOT_HOST"
 REQUIREMENTS_TXT="$PROJECT_ROOT_CONTAINER/requirements.txt"
 ON_DOCKER_STARTUP="$PROJECT_ROOT_CONTAINER/on_docker_startup.sh"
 
+TTY_FLAG=""
+if [ "$RUN_WITH_TTY" == "true" ]; then
+    TTY_FLAG="--tty"
+fi
 
 mkdir --parents "$WEST_WORKSPACE_HOST"
 
-docker build \
-    --network host \
-    --build-arg="UID=$USER_UID" \
-    --build-arg="GID=$USER_GID" \
-    --tag zephyr-box \
-    "$DOCKER_DIR"
+if [ "$RUN_LOCALLY" == "true" ]; then
+    # Build latest zephyr-box from scratch
+    docker build \
+        --network host \
+        --build-arg="UID=$USER_UID" \
+        --build-arg="GID=$USER_GID" \
+        --tag zephyr-box \
+        "$DOCKER_DIR"
+else
+    # Get zephyr-box image version from Git tag
+    IMAGE_VERSION=$(git describe --tags --abbrev=0 2>/dev/null | sed -n 's/^v\([0-9]\+\.[0-9]\+\).*/\1/p')
+    if [ -z "$IMAGE_VERSION" ]; then
+        printf "No valid Git tag found to determine the version of the Docker image to be pulled from the remote\n"
+        exit 1
+    fi
+    # Use already built zephyr-box image from remote with a tiny wrapper to get user UID and GID correct
+    docker build \
+         --build-arg="ZEPHYR_BOX_IMAGE=${DOCKER_REGISTRY}/$IMAGE_NAME:$IMAGE_VERSION" \
+         --build-arg="UID=$USER_UID" \
+         --build-arg="GID=$USER_GID" \
+         -t zephyr-box-user-wrapper \
+         -f "$DOCKER_DIR"/DockerfileUserWrapper .
+fi
 
 docker run \
-    --network host --tty --interactive --rm --privileged \
+    --network host $TTY_FLAG --interactive --rm --privileged \
     --volume "$WEST_WORKSPACE_HOST:$WEST_WORKSPACE_CONTAINER" \
     --volume "$PROJECT_ROOT_HOST:$PROJECT_ROOT_CONTAINER" \
-    --volume ~/.ssh:$HOME_CONTAINTER/.ssh \
+    --volume "$SSH_DIR":$HOME_CONTAINTER/.ssh \
     --volume /dev:/dev \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
     --env WEST_WORKSPACE="$WEST_WORKSPACE_CONTAINER" \
     --env PROJECT_ROOT="$PROJECT_ROOT_CONTAINER" \
     --env PYTHON_VENV="$PYTHON_VENV_CONTAINER" \
